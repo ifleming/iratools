5710acb6e9760a8505600f65b3b0803b2409e9be
Ira Fleming <irafleming@gmail.com>
2025-01-13 13:35:59 -0700

    change scale colors

--- R/heatmarkers.R
+++ R/heatmarkers.R
@@ -67,11 +67,11 @@ heatmarkers <- function(seurat_object,
     ggplot2::geom_tile() +
     ggplot2::facet_grid(. ~ group, scales = "free_x", space = "free_x") +
     ggplot2::scale_fill_gradientn(
-      colors = c("#313695", "#7196c6", "#e0e8ea", "#e1e1a4",
+      colors = c("#313695", "#7196c6", 
                 "#FEE090", "#FDAE61", "#F46D43", 
                 "#D73027", "#A50026"),
       limits = c(min(plot_data$expression), max(plot_data$expression)),
-      midpoint = (min(plot_data$expression) - max(plot_data$expression) + (min(plot_data$expression) - max(plot_data$expression))*0.2),
+      midpoint = (min(plot_data$expression) - max(plot_data$expression) + (min(plot_data$expression) - max(plot_data$expression))*0.1),
       name = "Scaled\nExpression"
     ) +
     ggplot2::theme_minimal() +
